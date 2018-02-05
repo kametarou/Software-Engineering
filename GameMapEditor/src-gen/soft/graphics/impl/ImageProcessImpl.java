9b5a2a9e0fa52cae800d4b13d513a1a52b83c951
Tsukino Furukawa <tsukino95@gmail.com>
2018-02-05 23:29:14 +0900

    move function convertToSWT into package graphics

--- GameMapEditor/src-gen/soft/graphics/impl/ImageProcessImpl.java
+++ GameMapEditor/src-gen/soft/graphics/impl/ImageProcessImpl.java
@@ -3,20 +3,23 @@
 package soft.graphics.impl;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.ComponentColorModel;
+import java.awt.image.DirectColorModel;
+import java.awt.image.IndexColorModel;
+import java.awt.image.WritableRaster;
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.EList;
-
 import org.eclipse.emf.ecore.EClass;
-
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.PaletteData;
+import org.eclipse.swt.graphics.RGB;
 
 import soft.graphics.GraphicsPackage;
 import soft.graphics.ImageProcess;
-
 import soft.mapping.AreaVector;
 
 /**
@@ -103,6 +106,70 @@ public class ImageProcessImpl extends MinimalEObjectImpl.Container implements Im
 		// TODO: implement this method
 		// Ensure that you remove @generated or mark it @generated NOT
 		throw new UnsupportedOperationException();
+	}
+
+	//convert AWT image into SWT image
+	//modify code in http://johndingus.blogspot.jp/2014/01/java-convert-bufferedimage-to-swt.html
+	public static ImageData convertToSWT(BufferedImage bufferedImage) {
+		if (bufferedImage.getColorModel() instanceof DirectColorModel) {
+			DirectColorModel colorModel = (DirectColorModel)bufferedImage.getColorModel();
+			PaletteData palette = new PaletteData(colorModel.getRedMask(), colorModel.getGreenMask(), colorModel.getBlueMask());
+			ImageData data = new ImageData(bufferedImage.getWidth(), bufferedImage.getHeight(), colorModel.getPixelSize(), palette);
+			for (int y = 0; y < data.height; y++) {
+				for (int x = 0; x < data.width; x++) {
+					int rgb = bufferedImage.getRGB(x, y);
+					int pixel = palette.getPixel(new RGB((rgb >> 16) & 0xFF, (rgb >> 8) & 0xFF, rgb & 0xFF));
+					data.setPixel(x, y, pixel);
+					if (colorModel.hasAlpha()) {
+						data.setAlpha(x, y, (rgb >> 24) & 0xFF);
+					}
+				}
+			}
+			return data;
+		} else if (bufferedImage.getColorModel() instanceof IndexColorModel) {
+			IndexColorModel colorModel = (IndexColorModel)bufferedImage.getColorModel();
+			int size = colorModel.getMapSize();
+			byte[] reds = new byte[size];
+			byte[] greens = new byte[size];
+			byte[] blues = new byte[size];
+			colorModel.getReds(reds);
+			colorModel.getGreens(greens);
+			colorModel.getBlues(blues);
+			RGB[] rgbs = new RGB[size];
+			for (int i = 0; i < rgbs.length; i++) {
+				rgbs[i] = new RGB(reds[i] & 0xFF, greens[i] & 0xFF, blues[i] & 0xFF);
+			}
+			PaletteData palette = new PaletteData(rgbs);
+			ImageData data = new ImageData(bufferedImage.getWidth(), bufferedImage.getHeight(), colorModel.getPixelSize(), palette);
+			data.transparentPixel = colorModel.getTransparentPixel();
+			WritableRaster raster = bufferedImage.getRaster();
+			int[] pixelArray = new int[1];
+			for (int y = 0; y < data.height; y++) {
+				for (int x = 0; x < data.width; x++) {
+					raster.getPixel(x, y, pixelArray);
+					data.setPixel(x, y, pixelArray[0]);
+				}
+			}
+			return data;
+		}else if (bufferedImage.getColorModel() instanceof ComponentColorModel) {
+            ComponentColorModel colorModel = (ComponentColorModel)bufferedImage.getColorModel();
+            PaletteData palette = new PaletteData(0x0000FF, 0x00FF00,0xFF0000);
+
+            ImageData data = new ImageData(bufferedImage.getWidth(), bufferedImage.getHeight(), colorModel.getPixelSize(), palette);
+
+            WritableRaster raster = bufferedImage.getRaster();
+            int[] pixelArray = new int[4];
+            for (int y = 0; y < data.height; y++) {
+                for (int x = 0; x < data.width; x++) {
+                    raster.getPixel(x, y, pixelArray);
+                    int pixel = palette.getPixel(new RGB(pixelArray[0], pixelArray[1], pixelArray[2]));
+                    data.setPixel(x, y, pixel);
+                    data.setAlpha(x, y, pixelArray[3]);
+                }
+            }
+            return data;
+        }
+		return null;
 	}
 
 	/**
--- GameMapEditor/src/soft/main/impl/MapDrawer.java
+++ GameMapEditor/src/soft/main/impl/MapDrawer.java
@@ -1,15 +1,8 @@
 package soft.main.impl;
 
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-
-import javax.imageio.ImageIO;
-
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.PaletteData;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
@@ -87,14 +80,14 @@ public class MapDrawer extends Canvas {
             			  System.out.print(r + "," + g + "," + b);
             		  gc.setBackground(new Color(c.getDevice(), r, g, b));
             		  gc.fillRectangle(realw/width*x, realh/height*y, realw/width, realh/height);
-            		  
+
             		  if (myMap.getCellFromCurrentLayer(x, y).getMyAsset() != null) {
             			  // TODO this method generate too many Image instance.
             			  //ImageData imgdate = new ImageData("images/flower.png");
-            			  ImageData imgdate = soft.fileio.impl.MyImageReaderImpl.convertToSWT(myMap.getCellFromCurrentLayer(x, y).getMyAsset().getMyBufferedImage());
+            			  ImageData imgdate = soft.graphics.impl.ImageProcessImpl.convertToSWT(myMap.getCellFromCurrentLayer(x, y).getMyAsset().getMyBufferedImage());
             			  if(imgdate!=null) {
             			  gc.drawImage(new org.eclipse.swt.graphics.Image(Display.getCurrent(), imgdate), 0, 0, 30, 30, realw/width*x, realh/height*y, realw/width, realh/height);
-            			  } 
+            			  }
             			  /*BufferedImage bf = myMap.getCellFromCurrentLayer(x, y).getMyAsset().getMyBufferedImage();
             			   PaletteData paletteData = new PaletteData(colors);
             			   ImageData imgdate = new ImageData("images/flower.ping");
@@ -102,16 +95,16 @@ public class MapDrawer extends Canvas {
             			   r2 = (rgb >> 16) & 0x000000FF;
             			   g2 = (rgb >>8 ) & 0x000000FF;
             			   b2 = (rgb) & 0x000000FF;
-            			   
+
             			   gc.setBackground(new Color(c.getDevice(), r2, g2, b2));
             			   gc.fillRectangle(realw/width*x, realh/height*y, realw/width, realh/height);*/
             			  /*ImageData imgdate = ConvertImage.convertToSWT(myMap.getCellFromCurrentLayer(x, y).getMyAsset().getMyBufferedImage());
             			  if (imgdate != null) {
-            				  org.eclipse.swt.graphics.Image myimg = new org.eclipse.swt.graphics.Image(Display.getCurrent(), imgdate); 
+            				  org.eclipse.swt.graphics.Image myimg = new org.eclipse.swt.graphics.Image(Display.getCurrent(), imgdate);
       						gc.drawImage(myimg, 0, 0, 5, 5, realw/width*x, realh/height*y, realw/width, realh/height);
       						System.out.println("test");
 						}*/
-            			  
+
 					}
             	  }
             }
